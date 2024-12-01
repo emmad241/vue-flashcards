7d56568fc77713bf8e09faf6f209b7742f839897
Emma Duffy <emma.duffy@mycit.ie>
2024-12-01 17:51:17 +0000

    allow adding of cards

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const app = Vue.createApp({
     data() {
         return {
             flashCards: [
-                { index: 1, question: 'What is the capital of France?', answer: 'Paris', show: true },
-                { index: 2, question: 'What is the capital of Spain?', answer: 'Madrid', show: true },
-                { index: 3, question: 'What is the capital of Italy?', answer: 'Rome', show: true },
-                { index: 4, question: 'What is the capital of Germany?', answer: 'Berlin', show: true },
+                { question: 'What is the capital of France?', answer: 'Paris', show: true },
+                { question: 'What is the capital of Spain?', answer: 'Madrid', show: true },
+                { question: 'What is the capital of Italy?', answer: 'Rome', show: true },
+                { question: 'What is the capital of Germany?', answer: 'Berlin', show: true },
             ],
+            newCard: { question: '', answer: '', show: true }
         }
     },
     methods: {
-        toggleCard(index) {
-            this.flashCards[index].show = !this.flashCards[index].show
+        toggleCard(card) {
+            card.show = !card.show
+        },
+        addCard() {
+            this.flashCards.push({ ...this.newCard })
+            this.newCard.question = ''
+            this.newCard.answer = ''
         }
     }
 })
