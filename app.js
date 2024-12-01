9175093dcc64ef63fefea31e258897db48bb673b
Emma Duffy <emma.duffy@mycit.ie>
2024-12-01 20:10:24 +0000

    implement All cards deck

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             decks: [
+                { category: 'All', cards: [] },
                 { category: 'Countries', cards: [
                     { question: 'What is the capital of France?', answer: 'Paris', show: true },
                     { question: 'What is the capital of Spain?', answer: 'Madrid', show: true },
@@ -21,52 +22,66 @@ const app = Vue.createApp({
                     { question: 'What color is the snow?', answer: 'White', show: true },
                 ]},
             ],
-            selectedDeck: { category: '', cards: [] },
+            selectedDeck: null,
             newCard: { question: '', answer: '', show: true },
             newDeck: { category: '', cards: [] }
         }
     },
     created() {
-        const decks = localStorage.getItem('decks')
-        if (decks) {
-            this.decks = JSON.parse(decks)
+        const savedDecks = localStorage.getItem('decks');
+        if (savedDecks) {
+            this.decks = JSON.parse(savedDecks);
         }
+        this.updateAllDeck();
+        this.selectedDeck = this.decks[0];
     },
     methods: {
         toggleCard(card) {
-            card.show = !card.show
-            this.saveCards()
+            card.show = !card.show;
         },
         addDeck() {
             if (this.newDeck.category) {
-                this.decks.push({ category: this.newDeck.category, cards: [] })
-                this.newDeck.category = ''
+                this.decks.push({ category: this.newDeck.category, cards: [] });
+                this.newDeck.category = '';
+                this.updateAllDeck();
             }
-            this.saveCards()
+            this.saveCards();
         },
         addCard() {
             if (this.newCard.question && this.newCard.answer) {
-                this.selectedDeck.cards.push({ ...this.newCard })
-                this.newCard.question = ''
-                this.newCard.answer = ''
+                this.selectedDeck.cards.push({ ...this.newCard });
+                this.newCard.question = '';
+                this.newCard.answer = '';
+                this.updateAllDeck();
             }
-            this.saveCards()
+            this.saveCards();
         },
         shuffleCards() {
-            this.selectedDeck.cards = this.selectedDeck.cards.sort(() => Math.random() - 0.5)
+            this.selectedDeck.cards = this.selectedDeck.cards.sort(() => Math.random() - 0.5);
         },
         removeCard(card) {
-            this.selectedDeck.cards = this.selectedDeck.cards.filter(c => c !== card)
-            this.saveCards()
+            this.selectedDeck.cards = this.selectedDeck.cards.filter(c => c !== card);
+            this.updateAllDeck();
+            this.saveCards();
         },
         removeDeck() {
-            this.decks = this.decks.filter(d => d !== this.selectedDeck)
-            this.saveCards()
+            this.decks = this.decks.filter(d => d !== this.selectedDeck);
+            this.updateAllDeck();
+            this.saveCards();
+            this.selectedDeck = this.decks[0];
+        },
+        updateAllDeck() {
+            const allDeck = this.decks.find(deck => deck.category === 'All');
+            if (allDeck) {
+                allDeck.cards = this.decks
+                    .filter(deck => deck.category !== 'All')
+                    .flatMap(deck => deck.cards);
+            }
         },
         saveCards() {
-            localStorage.setItem('decks', JSON.stringify(this.decks))
+            localStorage.setItem('decks', JSON.stringify(this.decks));
         }
     }
-})
+});
 
-app.mount('#app')
+app.mount('#app');
